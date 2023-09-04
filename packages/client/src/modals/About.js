c029efe3053d4658e889df60afea8dc56f925ecd
pisuthd <pisuth.dae@gmail.com>
2023-09-04 12:23:22 +0900

    [client] update about

--- packages/client/src/modals/About.js
+++ packages/client/src/modals/About.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Modal, Frame, Avatar } from '@react95/core'
 import { InfoBubble } from '@react95/icons'
-import styled from 'styled-components'; 
+import styled from 'styled-components';
 
 const Logo = styled.img.attrs(() => ({ src: "./logo.gif" }))`
     width: 220px;
@@ -35,21 +35,22 @@ const About = ({
                 width="100%"
                 padding="0px 5px"
             >
-                {/* <h3><i>RetroWeb3.Games</i></h3> */}
                 <p>
-                    RetroWeb3.Games collects the classic games and turns it into fully on-chain play-to-earn experiences that utilize the Oasys blockchain and zero-knowledge proof technology.
+                    RetroWeb3.Games is a platform for classic/retro/mini games in Web3 on the Oasys L-2 Sakuuru blockchain utilizing Merkle Tree verification for a fully on-chain experience and enjoys a secure and transparent gaming environment.
                 </p>
-                
                 <div style={{ display: "flex", flexDirection: "row" }}>
-                    <div style={{flex :1}}>
+                    <div style={{ flex: 1 }}>
                         <h4><i>Game Available</i></h4>
                         <ul>
-                            <li>Minesweeper - A puzzle game where you flag hidden mines with OAS tokens and the person who flags the last mine will win the prize pool.</li>
+                            <li>Minesweeper - A puzzle game where you flag hidden mines with Flag NFT and the person who flags the last mine will win the prize pool.</li>
+                            <li>Blackjack - A classic card game where the objective is to have a hand value as close to 21 as possible and compete against the dealer.</li>
+                            <li>Tomo Playground - A mini-game where Tomo's virtual pet can socialize with others and earn happiness points.</li>
                         </ul>
                     </div>
-                     
                 </div>
-
+                <p>
+                    
+                </p>
             </Frame>
         </Modal>
     )
